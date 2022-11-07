b3a0cb6cc75b34f0e9ddaf985e37210ec40adce5
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-11-07 08:25:00 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'bases';
+    titulo: string = 'Contador';
+    numero: number = 10;
+    base: number = 5;
+
+    acumular( num: number){
+        this.numero+= num;
+    }
+
 }
