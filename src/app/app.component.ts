f47dcefb3d0f1f318b8008688e95600efde0ff56
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-11-11 11:08:33 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
+
     templateUrl: './app.component.html',
+    
     styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
     titulo: string = 'Contador';
