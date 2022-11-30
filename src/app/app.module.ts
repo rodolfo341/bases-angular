04b6fdadaea69414ddc949b2e03098fded258ed8
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-11-30 07:55:46 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador.component';
+import { ContadorComponent } from './contador/contador.component';
 
 @NgModule({
   declarations: [
