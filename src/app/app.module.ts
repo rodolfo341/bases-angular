d869e98699a50ffb1706d96caf6d00bb99d8c20a
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-11-30 08:58:03 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent
+    ContadorComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/heroes/heroe/heroe.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeroeComponent } from './heroe.component';
+
+describe('HeroeComponent', () => {
+  let component: HeroeComponent;
+  let fixture: ComponentFixture<HeroeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HeroeComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HeroeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
