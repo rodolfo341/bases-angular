1881d6901abaf94e521739d4e2e22830b8ccab2f
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-12-25 19:34:17 -0300

    .

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
+import { HeroeComponent } from "./heroe/heroe.component";
+import { ListadoComponent } from "./listado/listado.component";
 
 @NgModule({
-    
+    declarations: [
+        HeroeComponent,
+        ListadoComponent
+    ]
 })
 export class HeroesModule{}
 
