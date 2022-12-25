6760969837dadb5dfb9628389a9eae7065e2d10e
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-12-25 19:55:18 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
     declarations: [
         AppComponent,
         ContadorComponent,
-        HeroeComponent,
-        ListadoComponent,
     ],
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        HeroesModule
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
