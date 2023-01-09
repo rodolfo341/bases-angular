9de6fbb15523ce12cc494a68b8f1f3b826610a6e
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2023-01-09 19:01:43 -0300

    .

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPageComponent } from './main-page/main-page.component';
+
+@NgModule({
+  declarations: [
+    MainPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DbzModule { }
